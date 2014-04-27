d82ed0a7fa3346158d369ca0561388eb1d1640d5
brittyu <yu.britt@gmail.com>
2014-04-27 08:27:48 +0800

    alter list.c(finish some function)

--- list.c
+++ list.c
@@ -39,5 +39,44 @@ int list_ins_next(List *list, ListElmt *element, const void *data) {
     }
     else {
         if (element->next == NULL)
+            list->tail = new_element;
+
+        new_element->next = element->next;
+        element->next = new_element;
     }
+    list->size++;
+
+    return 0;
+}
+
+int list_rem_next(List *list, ListElmt *element, void **data) {
+    ListElmt *old_element;
+
+    if (list_size(list) == 0)
+        return -1;
+
+    if (element == NULL) {
+        *data = list->head->data;
+        old_element = list->head;
+        list->head = list->head->next;
+
+        if (list_size(list) == 1)
+            list->tail = NULL;
+
+    }
+    else {
+        if (element->next == NULL)
+            return -1;
+
+        *data = element->next->data;
+        old_element = element->next;
+        element->next = element->next->next;
+
+        if (element->next == NULL)
+            list->tail = element;
+    }
+    free(old_element);
+
+    list->size--;
+    return 0;
 }
