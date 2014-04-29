aff375152f08177962203f41ff1fb83da2d4bf3e
brittyu <yu.britt@gmail.com>
2014-04-29 23:07:17 +0800

    add dlist function

--- list/list.c
+++ list/list.c
@@ -27,6 +27,7 @@ void list_destroy(List *list) {
 int list_ins_next(List *list, ListElmt *element, const void *data) {
     ListElmt *new_element;
 
+    //create new element.
     if ((new_element = (ListElmt *)malloc(sizeof(ListElmt))) == NULL)
         return -1;
 
@@ -55,16 +56,19 @@ int list_rem_next(List *list, ListElmt *element, void **data) {
     if (list_size(list) == 0)
         return -1;
 
+    //if the element is NULL,remove the head.
     if (element == NULL) {
         *data = list->head->data;
         old_element = list->head;
         list->head = list->head->next;
 
+        //if the size of the list is 0,the tail is NULL.
         if (list_size(list) == 1)
             list->tail = NULL;
 
     }
     else {
+        //if element is the tail,return error.
         if (element->next == NULL)
             return -1;
 
@@ -72,9 +76,11 @@ int list_rem_next(List *list, ListElmt *element, void **data) {
         old_element = element->next;
         element->next = element->next->next;
 
+        //if element is the tail,set the tail as element.
         if (element->next == NULL)
             list->tail = element;
     }
+    //extract the old_element and free it.
     free(old_element);
 
     list->size--;
