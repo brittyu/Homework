c909cabac82afbf075d7614b61b0de871e1567b1
brittyu <yu.britt@gmail.com>
2015-11-26 16:50:45 +0800

    修改注释

--- Python/wangluoanquan/server/app/mysql.py
+++ Python/wangluoanquan/server/app/mysql.py
@@ -1,13 +1,14 @@
 #! /usr/env python
 # -*- coding: utf-8 -*-
 
+"""mysql数据库操作"""
+
 import MySQLdb
 import sys
 
-
 class Mysql:
+    """数据库基本操作"""
 
-    # 初始化连接数据库操作处理
     def __init__(self):
         try:
             self.con = MySQLdb.connect('localhost', 'homestead', 'secret', 'wangluoanquan')
@@ -15,8 +16,8 @@ class Mysql:
             print "Error %d: %s" % (e.args[0], e.args[1])
             sys.exit(1)
 
-    # 检查是否数据库中存在要查询的用户
     def make_query(self, name, pd):
+        """进行数据库查询工作"""
         cur = self.con.cursor()
         cur.execute("select * from user where username=%s and password=%s", (name, pd))
 
